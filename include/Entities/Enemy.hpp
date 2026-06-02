fded063680a4987981b70d61ad3e2066a50ff47b
neoxidin <vanya.robotov@gmail.com>
2026-06-02 20:17:40 +0300

    Update Enemy.hpp

--- include/Entities/Enemy.hpp
+++ include/Entities/Enemy.hpp
@@ -3,23 +3,35 @@
 
 #pragma once
 
+#include <utility>
+
 namespace Entities {
+
   class Enemy {
-    // Поля класса:
-    // координаты, здоровье максимальное и текущее, уровень, скорость
+    private:
+      int x_;
+      int y_;
+      int hp_current_;
+      int hp_max_;
+      int level_;
+      float speed_;
 
-    // Методы класса (изменение):
-    // move() - перемещение по x, y
-    // add_hp() - добавление текущего здоровья на k единиц, но не более максимального
-    // set_hp() - изменение максимального здоровья
-    // set_lvl() - изменение уровня противника
+    public:
+        // Конструктор
+      Enemy(int x, int y, int hp_max, int level, float speed);
 
-    // Методы класса (вывод):
-    // pos() - получение координат x, y в виде pair<int,int>
-    // hp() - получение текущего ХП
-    // lvl() - получение уровня
-    // speed() - получение скорости
+        // Методы изменения состояния
+      void move(int dx, int dy);
+      void add_hp(int k);
+      void set_hp(int new_hp_max);
+      void set_lvl(int new_level);
+
+        // Методы получения состояния
+      std::pair<int, int> pos() const;
+      int hp() const;
+      int lvl() const;
+      float speed() const;
   };
-}
+} // namespace Entities
 
 #endif // ENEMY_HPP_

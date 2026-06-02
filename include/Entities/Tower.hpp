3ff6db7e9ed0ef5c4a6aee7669076f56298f4348
neoxidin <vanya.robotov@gmail.com>
2026-06-02 20:19:16 +0300

    Update Tower.hpp

--- include/Entities/Tower.hpp
+++ include/Entities/Tower.hpp
@@ -4,23 +4,37 @@
 #pragma once
 
 namespace Entities {
-  class Tower {
-    // Поля класса:
-    // уровень, урон, скорость, радиус атаки, стоимость
 
-    // Методы класса (изменение):
-    // add_lvl() - добавление 1 к уровню
-    // set_damage() - изменение урона
-    // set_speed() - изменение скорострельности
-    // set_radius() - изменение радиуса
-    // set_money() - изменение стоимости (для улучшений)
+    class Tower {
+    private:
+        int hp_current_;
+        int hp_max_;
+        int damage_;
+        int speed_;
+        int radius_;
+        int cost_;
+        int level_;
 
-    // Методы класса (вывод):
-    // lvl() - получение уровня
-    // money() - получение стоимости
-    // damage() - получение урона
-    // radius() - получение радиуса
-  };
-}
+    public:
+        // Конструктор
+        Tower(int hp_max, int damage, int speed, int radius, int cost, int level);
+
+        // Методы изменения состояния
+        void add_lvl();
+        void set_damage(int new_damage);
+        void set_speed(int new_speed);
+        void set_radius(int new_radius);
+        void set_cost(int new_cost);
+
+        // Методы получения состояния
+        int lvl() const;
+        int cost() const;
+        int damage() const;
+        int radius() const;
+        int speed() const;
+        int hp() const;
+    };
+
+} // namespace Entities
 
 #endif // TOWER_HPP_
